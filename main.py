8452e21d64cdf7cb7d78c5c729079f756cebdd7c
BlackTea-c <2286554510@qq.com>
2023-12-08 20:11:41 +0800

    20:11

--- main.py
+++
@@ -1,6 +0,0 @@
-
-import numpy as np
-str='abcd'
-m=3
-n=1
-np.zeros((m,n))
--- 第三章 高质量的代码/eg 17 打印从1到最大的n位数.py
+++ 第三章 高质量的代码/eg 17 打印从1到最大的n位数.py
@@ -13,3 +13,40 @@ def f(n): #n>=1
     for i in range(1,max+1):
         print(i)
 #f(10)
+
+
+
+
+
+#用全排列的方法解决,递归
+
+def printToMaxOfNDigits(n):
+    if n <= 0:
+        return
+
+    number = ['0'] * n #初始化n位数
+    for i in range(10):
+        number[0] = str(i)
+        printToMaxOfNDigitsRecursively(number, n, 0)
+
+def printNumber(number): #省略为0的开头 00000102 从第一个不为0的开始打印
+    isBeginning0 = True
+    for i in range(len(number)):
+        if isBeginning0 and number[i] != '0':
+            isBeginning0 = False
+        if not isBeginning0:
+            print(number[i], end='')
+    print()
+
+def printToMaxOfNDigitsRecursively(number, length, index):
+    if index == length - 1:
+        printNumber(number)
+        return
+
+    for i in range(10):
+        number[index + 1] = str(i)
+        printToMaxOfNDigitsRecursively(number, length, index + 1)
+
+# 测试示例
+n = 3
+printToMaxOfNDigits(n)
